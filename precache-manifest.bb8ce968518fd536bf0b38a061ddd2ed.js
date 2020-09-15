fdfeae01d92215547b6e6aa8a6b55b91169f43c1
mark-nikerin <mrnikerin@gmail.com>
2020-09-15 13:22:21 +0400

    Updates

---
+++ precache-manifest.bb8ce968518fd536bf0b38a061ddd2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00db0b915e1e0b5db8482065722fe3e",
+    "url": "/vezdekod-mini-app/index.html"
+  },
+  {
+    "revision": "d2e3a4ac0e144889ae82",
+    "url": "/vezdekod-mini-app/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "d2e3a4ac0e144889ae82",
+    "url": "/vezdekod-mini-app/static/js/2.b6061636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vezdekod-mini-app/static/js/2.b6061636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b79b602ec4095cd2ef",
+    "url": "/vezdekod-mini-app/static/js/main.84c6c901.chunk.js"
+  },
+  {
+    "revision": "516bacc3f443be0115f3",
+    "url": "/vezdekod-mini-app/static/js/runtime-main.c4cd9424.js"
+  }
+]);
